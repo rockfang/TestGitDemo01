8c468bdea354a169045abe65c04346aa6d96907f
caifang <cooldish1245@163.com>
2018-07-13 18:19:09 +0800

    修改02

--- app/src/main/java/wostore/infinit/com/testgitdemo0713/MainActivity.java
+++ app/src/main/java/wostore/infinit/com/testgitdemo0713/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //2222
+        //201807121818
     }
 }
